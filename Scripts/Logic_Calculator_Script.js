6a984332fb679378a8d8ecfbf02965cfc0a859cb
II-ELIF-II <Blackjack1000k@gmail.com>
2020-03-08 23:12:29 +0800

    Update Logic_Calculator_Script.js

--- Scripts/Logic_Calculator_Script.js
+++ Scripts/Logic_Calculator_Script.js
@@ -18,11 +18,9 @@ function quiz(ques)
 {
     var x, text;
 
-    // Get the value of the input field with id="numb"
     x = document.getElementById("test-text").value;
     if(ques=1)
     {
-    // If x is Not a Number or less than one or greater than 10
         if (x==="True") 
         { text = "Input not valid"; } 
         else 
